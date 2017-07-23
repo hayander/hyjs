2d0ecd167cbf3fdb5ab2f655d1f011fc854e9300
Hayden Anderson <hayander@gmail.com>
2017-07-23 19:22:39 +1000

    Modified tests

--- test/unit/core/core.js
+++ test/unit/core/core.js
@@ -19,12 +19,17 @@ suite('Core', function() {
         test('Should create instance of hy', function() {
             assert.instanceOf(hyI, hy);
         });
-        test('_initialised should be true', function () {
-            assert.isOk(hyI._initialised);
+        test('_hyInitialised should be true', function () {
+            assert.isOk(hyI._hyInitialised);
         });
-
         test('Display is an instance of hy.Display', function() {
             assert.instanceOf(hyI._display, hy.Display);
+        });
+        test('Loaded in global mode', function() {
+            assert.equal(hyI._global, true);
+        });
+        test('_hyInitialised should be available on window (Global scope)', function() {
+            assert.equal(hyI._hyInitialised, true);
         });
 
     });
